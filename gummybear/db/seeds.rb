00c877a98a1d59f09c6e2112e73373dfc748e390
Ben DiPaola & jdsthird <pair+bdipaola.jdsthird@devbootcamp.com>
2015-08-03 11:37:39 -0500

    begin seeding

--- gummybear/db/seeds.rb
+++ gummybear/db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#Destroying all records
+User.destroy_all
+Game.destroy_all
+Category.destroy_all
+Friending.destroy_all
+UserGame.destroy_all
+
+#Creating seed users
+User.create!(name: "Nic", username: "Nic", email: "nic@nic.com", password_hash:)
+User.create!(name: "Christine", username: "Christine", email: "christine@christine.com", password_hash:)
+User.create!(name: "John", username: "John", email: "john@john.com", password_hash:)
+User.create!(name: "Ben", username: "Ben", email: "ben@ben.com", password_hash:)
+User.create!(name: "Brockenbar", username: "Brockenbar", email: "Brockenbar@kot.com", password_hash:)
+User.create!(name: "Space Penguin", username: "SpacePenguin", email: "SpacePenguin@kot.com", password_hash:)
+User.create!(name: "Boogey Woogey", username: "BoogeyWoogey", email: "BoogeyWoogey@kot.com", password_hash:)
+User.create!(name: "Pumpkin Jack", username: "Pumpkin Jack", email: "PumpkinJack@kot.com", password_hash:)
+
+#Creating seed categories
+negotiation = Category.create!(name: "Negotiation")
+card = Category.create!(name: "Card game")
+fantasy = Category.create!(name: "Fantasy")
+mythology = Category.create!(name: "Mythology")
+party = Category.create!(name: "Party Games")
+
+#Creating Games
+Game.create!(name: "Diplomacy", category: negotiation, min_players: 7, max_players: 7)
+Game.create!(name: "Oh My God! There's an Axe in My Head.", category: negotiation, min_players: 2, max_players: 6)
+Game.create!(name: "Diplomacy", category: negotiation, min_players: 7, max_players: 7)
+Game.create!(name: "Zombie State: Diplomacy of the Dead", category: negotiation, min_players: 2, max_players: 5)
+Game.create!(name: "Diplomacy", category: negotiation, min_players: 7, max_players: 7)
+Game.create!(name: "Diplomacy", category: negotiation, min_players: 7, max_players: 7)
