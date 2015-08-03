f159fa33165f9762eb88156217d3472ad7ea1f60
John Sternberg <jdsthird@gmail.com>
2015-08-03 14:42:03 -0500

    add login methods to User class

--- gummybear/app/models/user.rb
+++ gummybear/app/models/user.rb
@@ -7,4 +7,19 @@ class User < ActiveRecord::Base
 
   has_many :friendings
   has_many :friends, through: :friendings
+
+
+  def password
+    @password ||= BCrypt::Password.new(self.password_hash)
+  end
+
+  def password=(raw_password)
+    @password = BCrypt::Password.create(raw_password)
+    self.password_hash = @password
+  end
+
+  def valid_login?(raw_password)
+    password == raw_password
+  end
+
 end
