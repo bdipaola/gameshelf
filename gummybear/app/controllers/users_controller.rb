de556b13b5fcf0307f8a70913059cd79835f9533
Christine Hager <cehager@gmail.com>
2015-08-03 20:44:04 -0500

    Add create user, working on dashboard authentication

--- gummybear/app/controllers/users_controller.rb
+++ gummybear/app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+  include LoginHelper
 
-  def index
+  def dashboard
+    # enforce_login(params[:id], "/")
     @games = Game.all
   end
 
@@ -8,4 +10,22 @@ class UsersController < ApplicationController
     @games = Game.all
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      redirect_to "/users/#{@user.id}/dashboard"
+    else
+      render :new
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :username, :email, :password)
+    end
 end
--- gummybear/config/routes.rb
+++ gummybear/config/routes.rb
@@ -6,12 +6,13 @@ Rails.application.routes.draw do
   root 'games#index'
 
   # Example of regular route:
-    # get 'users/dashboard' => 'users#dashboard'
+    get 'users/:id/dashboard' => 'users#dashboard'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
+
   resources :users do
     resources :games
   end
