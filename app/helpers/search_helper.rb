d808bdc8c829d8d402a78b98fb70ae0ab3426c5e
John Sternberg <jdsthird@gmail.com>
2015-08-04 19:15:59 -0500

    add basic canonical searches

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -3,7 +3,19 @@ module SearchHelper
     User.find_by(username: username).order(:name)
   end
 
-  def user_name_search(name)
-    User.find_by(name: name).order(:name)
+  def user_canonical_username_search(query_term)
+    User.where("canonical_username like ?", "%#{query_term.downcase}%").order(:name)
+  end
+
+  def user_canonical_name_search(query_term)
+    User.where("canonical_name like ?", "%#{query_term.downcase}%").order(:name)
+  end
+
+  def game_canonical_name_search(query_term)
+    Game.where("canonical_name like ?", "%#{query_term.downcase}%").order(:name)
+  end
+
+  def category_canonical_name_search(query_term)
+    Category.where("canonical_name like ?", "%#{query_term.downcase}%").order(:name)
   end
 end
