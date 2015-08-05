0529b74a7ee923a15b7c9efe91d4bf96b60e7f74
John Sternberg <jdsthird@gmail.com>
2015-08-04 19:52:40 -0500

    implement search helpers

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,16 @@
 class SearchController < ApplicationController
+  include SearchHelper
 
   def index
   end
 
   def create
-    game = Game.where("name like ?", "%#{params[:search]}%").first
-    if game
-      redirect_to game
+    # game = Game.where("name like ?", "%#{params[:search]}%").first
+    games = game_search(params[:search])
+    users = user_search(params[:search])
+    categories = category_search(params[:search])
+    if games
+      # redirect_to categories.first
     else
       redirect_to search_index_path
     end
--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -18,4 +18,38 @@ module SearchHelper
   def category_canonical_name_search(query_term)
     Category.where("canonical_name like ?", "%#{query_term.downcase}%").order(:name)
   end
+
+  def game_search(query)
+    terms = query.split(' ')
+    results = []
+    terms.each do |term|
+      results += game_canonical_name_search(term)
+    end
+    sort_by_name(results.uniq)
+  end
+
+  def user_search(query)
+    terms = query.split(' ')
+    results = []
+    terms.each do |term|
+      results += user_canonical_name_search(term)
+      results += user_canonical_username_search(term)
+    end
+    sort_by_name(results.uniq)
+  end
+
+  def category_search(query)
+    terms = query.split(' ')
+    results = []
+    terms.each do |term|
+      results += category_canonical_name_search(term)
+    end
+    sort_by_name(results.uniq)
+  end
+
+  def sort_by_name(object_array)
+    object_array.uniq.sort do |a, b|
+      a.name <=> b.name
+    end
+  end
 end
