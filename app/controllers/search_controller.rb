dba17b0b6ecb01bdcbdb26295ad3d1dce5cbc61f
John Sternberg <jdsthird@gmail.com>
2015-08-04 20:34:50 -0500

    render games from search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,15 +2,11 @@ class SearchController < ApplicationController
   include SearchHelper
 
   def index
-  end
-
-  def create
     # game = Game.where("name like ?", "%#{params[:search]}%").first
-    games = game_search(params[:search])
-    users = user_search(params[:search])
-    categories = category_search(params[:search])
-    if games
-      # redirect_to categories.first
+    @games = game_search(params[:search])
+    @users = user_search(params[:search])
+    @categories = category_search(params[:search])
+    if @games
     else
       redirect_to search_index_path
     end
