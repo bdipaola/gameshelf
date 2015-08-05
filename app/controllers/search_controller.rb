1e0d8bb578f82acce9646643c6149f6c997f0d81
John Sternberg <jdsthird@gmail.com>
2015-08-04 21:20:17 -0500

    add search by player count to view/controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,12 +2,15 @@ class SearchController < ApplicationController
   include SearchHelper
 
   def index
-    @games = game_search(params[:search])
-    @users = user_search(params[:search])
-    @categories = category_search(params[:search])
-    if @games
+    num_players = params[:search].to_i
+    if num_players
+      @games = player_count_search(num_players)
+      @categories = []
+      @users = []
     else
-      redirect_to search_index_path
+      @games = game_search(params[:search])
+      @users = user_search(params[:search])
+      @categories = category_search(params[:search])
     end
   end
 end
